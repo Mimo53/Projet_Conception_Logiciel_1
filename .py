5e28349ce463b511f18a86a1d41998aeedbc31bf
Andre-Raymond <andre-raymond.socard@eleve.ensai.fr>
2025-01-27 16:39:26 +0100

    Delete .py

--- .py
+++
@@ -1,5 +0,0 @@
-# Faut créer des classes la teams et pour ça faire l'analyse fonctionnelle avant
-# Mais askip dungeo le fait bien
-
-
-# si c ton vieux fichier qui encule tout momo je vais t'enculer
