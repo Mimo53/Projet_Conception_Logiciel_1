421a985405698a58ca4f21531fc39861de97a9d3
Andre-raymond <andre-raymond.socard@eleve.ensai.fr>
2025-02-18 21:59:53 +0100

    juste un init

--- test.py
+++ test.py
@@ -4,3 +4,18 @@
 print("Pitié")
 
 # Une seule ligne vide à la fin du fichier
+from dotenv import load_dotenv
+import os
+
+# Charger le fichier .env
+load_dotenv()
+if os.path.exists('.env'):
+    print(".env file found!")
+else:
+    print(".env file not found!")
+# Vérifier les variables
+print(f"Loaded DATABASE_USER: {os.getenv('DATABASE_USER')}")
+print(f"Loaded DATABASE_PASSWORD: {os.getenv('DATABASE_PASSWORD')}")
+print(f"Loaded DATABASE_HOST: {os.getenv('DATABASE_HOST')}")
+print(f"Loaded DATABASE_PORT: {os.getenv('DATABASE_PORT')}")
+print(f"Loaded DATABASE_NAME: {os.getenv('DATABASE_NAME')}")
