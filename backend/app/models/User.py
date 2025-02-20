9894407ce302f90ec29011adc40ef6a02fbd2ffc
Dorian <dorian.pubs@gmail.com>
2025-02-20 23:09:42 +0100

    user mieux merge

--- backend/app/models/User.py
+++ backend/app/models/User.py
@@ -1,28 +1,30 @@
 from pydantic import BaseModel, EmailStr
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
-from ..db.database import Base, get_db
-from ..models.Enums import Role
+from backend.app.db.database import Base, get_db
+from backend.app.models.Enums import Role
 
 
 class User(Base):
     __tablename__ = 'User'
 
-    username = Column(String, primary_key=True,index=True)
+    username = Column(String, primary_key=True, index = True)
     password = Column(String, index=True)
     role = Column(String, index=True)
     e_mail = Column(String, index=True)
 
-    user_cards = relationship("UserCard", back_populates="user")
+    user_cards = relationship("UserCard", back_populates =" user")
     
 
 class UserBase(BaseModel):
     username: str 
     password: str 
-    role: str 
+    role: Role 
     e_mail: EmailStr | None = None 
+
 
 class UserUpdate(BaseModel):
     new_username: str | None = None
     new_password: str | None = None
-    new_email: EmailStr | None = None 
+    new_email: EmailStr | None = None
