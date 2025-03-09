8d018bfe2f72c2bbc108ad612d9b946fc3d917b7
Andre-raymond <andre-raymond.socard@eleve.ensai.fr>
2025-03-09 03:17:06 +0100

    test

--- backend/app/models/mail.py
+++ backend/app/models/mail.py
@@ -9,14 +9,13 @@ from fastapi_mail import ConnectionConfig
 
 load_dotenv()
 
-conf= ConnectionConfig(
-    MAIL_USERNAME = os.getenv("MAIL_USERNAME"),
-    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD"),
-    MAIL_FROM = os.getenv("MAIL_USERNAME"),
-    MAIL_PORT = 587,
+conf = ConnectionConfig(
+    MAIL_USERNAME=os.getenv("MAIL_USERNAME", "default_username"),
+    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD", "default_password"),
+    MAIL_FROM=os.getenv("MAIL_FROM", "default_email@example.com"),
+    MAIL_PORT=587,
     MAIL_SERVER="smtp.gmail.com",
     MAIL_STARTTLS=True,
     MAIL_SSL_TLS=False,
     USE_CREDENTIALS=True
-
 )
