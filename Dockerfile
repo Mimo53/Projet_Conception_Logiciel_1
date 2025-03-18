2fe0ecab7567de702de2ab21b4d6ee75269e1558
Andre-raymond <andre-raymond.socard@eleve.ensai.fr>
2025-03-18 13:25:19 +0100

    docker

--- Dockerfile
+++ Dockerfile
@@ -13,8 +13,3 @@ COPY . .
 
 # Exposer le port FastAPI
 EXPOSE 8000
-
-# Lancer l'application avec Uvicorn
-CMD ["fastapi", "dev", "backend/main.py"]
-
-# docker run --name fastapi_backend --env-file .env -p 8000:8000 fastapi_app
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000',  // L'adresse de ton backend FastAPI
-        changeOrigin: true,
-        secure: false
+export default defineConfig(({ mode }) => {
+  // Charger les variables d'environnement depuis le fichier .env
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      host: true,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,  // Utilisation correcte de la variable d'environnement
+          changeOrigin: true,
+          secure: false
+        }
       }
     }
   }
